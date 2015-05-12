88062fe15b4a397deafcdc0ae16dc05c48ce453c
Codemis <johnathan@jpulos.com>
2015-05-12 12:01:04 -0700

    Misspelled the construct method.

--- Extension.php
+++ Extension.php
@@ -44,7 +44,7 @@ class Extension extends BaseExtension
      **/
     public function __construct(Application $app)
     {
-        parent::construct($app);
+        parent::__construct($app);
         $this->app['config']->getFields->addField(new PiconSelectorField());
         if ($this->app['config']->getWhichEnd() == 'backend') {
             $this->app['htmlsnippets'] = true;
